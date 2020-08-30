51b1126350c6d4e6fe984b1d4a32de0ec77e2f20
Lycolia <sazanami@iona.2-d.jp>
2020-08-30 21:01:50 +0900

    first commit

---
+++ mysqlconnector.py
@@ -0,0 +1,13 @@
+import os
+import mysql.connector
+
+class MySqlConnector:
+    def __init__(self):
+        self.conn = mysql.connector.connect(
+            option_files = os.path.join(os.path.dirname(__file__), 'mysql.conf')
+        )
+        self.cur = self.conn.cursor()
+
+    def close(self):
+        self.cur.close()
+        self.conn.close()
