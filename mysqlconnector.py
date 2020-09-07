0de70d92a836df96a9a5986fd01c9d44a5a46aae
Lycolia <sazanami@iona.2-d.jp>
2020-09-07 21:49:11 +0900

    autoformat

--- mysqlconnector.py
+++ mysqlconnector.py
@@ -1,10 +1,11 @@
 import os
 import mysql.connector
 
+
 class MySqlConnector:
     def __init__(self):
         self.conn = mysql.connector.connect(
-            option_files = os.path.join(os.path.dirname(__file__), 'mysql.conf')
+            option_files=os.path.join(os.path.dirname(__file__), 'mysql.conf')
         )
         self.cur = self.conn.cursor()
 
